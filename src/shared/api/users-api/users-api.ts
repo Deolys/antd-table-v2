d2393fe84f9bb293393146c2a878a18f7b24baf6
Deolys <ed03aleks@mail.ru>
2024-10-01 21:10:36 +0300

    feat: add real api

--- src/shared/api/users-api/users-api.ts
+++ src/shared/api/users-api/users-api.ts
@@ -1,132 +1,83 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import dayjs from 'dayjs';
-import localforage from 'localforage';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import type { CreateUser, UpdateUser, User, UserType, UsersFilters } from '@/shared/types/user';
 
-import userTypesData from '../../config/data/UserTypes.json';
-import usersData from '../../config/data/Users.json';
+interface Result {
+  data: [] | null;
+  error: { message: string; status: number } | null;
+}
 
-const USERS_KEY = 'users';
-const USER_TYPES_KEY = 'userTypes';
+export const usersApi = createApi({
+  reducerPath: 'usersApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  tagTypes: ['user', 'users', 'userTypes'],
+  endpoints: (builder) => ({
+    getFilteredUsers: builder.query<User[], UsersFilters>({
+      query: ({ name, type_id, dateRange }) => {
+        const queryParams = new URLSearchParams({
+          name: name || '',
+          type_id: type_id ? type_id.toString() : '',
+          dateRange: dateRange?.length ? JSON.stringify(dateRange) : '',
+        });
 
-const initUsers = createAsyncThunk('users/initUsers', async () => {
-  const data = await localforage.getItem(USERS_KEY);
-  if (!data) {
-    const usersWithTypes = usersData.map((user) => {
-      const userType = userTypesData.find((type) => type.id === user.type_id);
-      return { ...user, type: userType?.name };
-    });
+        return `/users?${queryParams.toString()}`;
+      },
+      providesTags: (result) =>
+        result && result.length > 0
+          ? [...result.map(({ _id }) => ({ type: 'users' as const, id: _id })), 'users']
+          : ['users'],
+    }),
 
-    await localforage.setItem(USERS_KEY, usersWithTypes);
-    await localforage.setItem(USER_TYPES_KEY, userTypesData);
+    getUserById: builder.query<User, string>({
+      query: (id) => `/users/${id}`,
+      providesTags: (result, error, id) => [{ type: 'user' as const, id }],
+    }),
 
-    return { usersWithTypes, userTypesData };
-  }
+    createUser: builder.mutation<User, CreateUser>({
+      query: (user) => ({
+        url: '/users',
+        method: 'POST',
+        body: user,
+      }),
+      invalidatesTags: ['users'],
+    }),
+
+    updateUser: builder.mutation<User, UpdateUser>({
+      query: (user) => ({
+        url: `/users/${user._id}`,
+        method: 'PUT',
+        body: user,
+      }),
+      invalidatesTags: (result, error, user) => [{ type: 'user' as const, id: user._id }],
+    }),
+
+    deleteUsers: builder.mutation<Result, number[]>({
+      query: (ids) => ({
+        url: '/users',
+        method: 'DELETE',
+        body: ids,
+      }),
+      invalidatesTags: (result) =>
+        result && result.data
+          ? [...result.data.map((id) => ({ type: 'users' as const, id: id })), 'users']
+          : ['users'],
+    }),
+
+    getUserTypes: builder.query<UserType[], void>({
+      query: () => '/user-types',
+      providesTags: (result) =>
+        result && result.length > 0
+          ? [...result.map(({ id }) => ({ type: 'userTypes' as const, id })), 'userTypes']
+          : ['userTypes'],
+    }),
+  }),
 });
 
-const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-  const users: User[] = (await localforage.getItem(USERS_KEY)) || [];
-  const userTypes: UserType[] = (await localforage.getItem(USER_TYPES_KEY)) || [];
-  const usersWithTypes = users.map((user) => {
-    const userType = userTypes.find((type) => type.id === user.type_id);
-    return { ...user, type: userType?.name };
-  });
-
-  return usersWithTypes;
-});
-
-const fetchUserTypes = createAsyncThunk('users/fetchUserTypes', async () => {
-  return (await localforage.getItem(USER_TYPES_KEY)) || [];
-});
-
-const fetchUserById = createAsyncThunk('users/fetchUserById', async (id: number) => {
-  const users: User[] = (await localforage.getItem(USERS_KEY)) || [];
-  const user = users.find((u) => u.id === id) || null;
-
-  return user;
-});
-
-const fetchFilteredUsers = createAsyncThunk(
-  'users/fetchFilteredUsers',
-  async (filters: UsersFilters) => {
-    const { name, type_id, dateRange } = filters;
-    const users: User[] = (await localforage.getItem(USERS_KEY)) || [];
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    const lowerCaseName = name ? name.toLowerCase() : null;
-    const validDateFrom = dayjs(dateRange[0]).isValid() ? dayjs(dateRange[0]).valueOf() : null;
-    const validDateTo = dayjs(dateRange[1]).isValid() ? dayjs(dateRange[1]).valueOf() : null;
-
-    const filteredUsers = users.filter((user) => {
-      const userLastVisitTime = dayjs(user.last_visit_date).valueOf();
-
-      return (
-        (!lowerCaseName || user.name.toLowerCase().includes(lowerCaseName)) &&
-        (!type_id || user.type_id === type_id) &&
-        (!validDateFrom || userLastVisitTime >= validDateFrom) &&
-        (!validDateTo || userLastVisitTime <= validDateTo)
-      );
-    });
-
-    const userTypes: UserType[] = (await localforage.getItem(USER_TYPES_KEY)) || [];
-    const usersWithTypes = filteredUsers.map((user) => {
-      const userType = userTypes.find((type) => type.id === user.type_id);
-      return { ...user, type: userType?.name };
-    });
-
-    return usersWithTypes;
-  },
-);
-
-const createUser = createAsyncThunk('users/createUser', async (user: CreateUser) => {
-  const users = (await localforage.getItem<User[]>(USERS_KEY)) || [];
-  const duplicateLoginIndex = users.findIndex((u) => u.login === user.login);
-  if (duplicateLoginIndex !== -1) {
-    throw new Error('Пользователь с указанным логином уже существует');
-  }
-  const newId = users.length > 0 ? Math.max(...users.map((u) => u.id)) + 1 : 1;
-  const lastVisitDate = dayjs()
-    .toISOString()
-    .replace(/\.\d+Z$/, '');
-  users.push({ id: newId, ...user, last_visit_date: lastVisitDate });
-  await localforage.setItem(USERS_KEY, users);
-
-  return users;
-});
-
-const updateUser = createAsyncThunk('users/updateUser', async (user: UpdateUser) => {
-  const users = (await localforage.getItem<User[]>(USERS_KEY)) || [];
-  const userIndex = users.findIndex((u) => u.id === user.id);
-  if (userIndex === -1) {
-    throw new Error(`Пользователь с id ${user.id} не найден`);
-  }
-  const duplicateLoginIndex = users.findIndex((u) => u.login === user.login && u.id !== user.id);
-  if (duplicateLoginIndex !== -1) {
-    throw new Error('Пользователь с указанным логином уже существует');
-  }
-  users[userIndex] = { ...users[userIndex], ...user };
-  console.log('user update: ', user);
-  await localforage.setItem(USERS_KEY, users);
-
-  return users;
-});
-
-const deleteUsers = createAsyncThunk('users/deleteUsers', async (ids: number[]) => {
-  const users = (await localforage.getItem<User[]>(USERS_KEY)) || [];
-  const updatedUsers = users.filter((u) => !ids.includes(u.id));
-  await localforage.setItem(USERS_KEY, updatedUsers);
-
-  return updatedUsers;
-});
-
-export const usersApi = {
-  initUsers,
-  fetchUsers,
-  fetchUserTypes,
-  fetchUserById,
-  fetchFilteredUsers,
-  createUser,
-  updateUser,
-  deleteUsers,
-};
+export const {
+  useGetFilteredUsersQuery,
+  useGetUserByIdQuery,
+  useCreateUserMutation,
+  useUpdateUserMutation,
+  useDeleteUsersMutation,
+  useGetUserTypesQuery,
+} = usersApi;
